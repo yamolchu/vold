25012a18f380ae7b1bb3fe87a9c4cf63bf132bb8
abobi228 <141289130+abobi228@users.noreply.github.com>
2023-08-31 00:11:08 +0300

    first commit

---
+++ inputs/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  numThreads: 4,
+  ref: '',
+  proxyType: 'http', // socks5 http
+  customDelay: 2000,
+};
+
+module.exports = config;
---
+++ src/index.ts
@@ -0,0 +1,128 @@
+const { random } = require('user-agents');
+const axios = require('axios');
+const { SocksProxyAgent } = require('socks-proxy-agent');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+const fs = require('fs');
+const { Worker, workerData, isMainThread, parentPort } = require('worker_threads');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const config = require('../inputs/config.ts');
+
+const csvWriter = createCsvWriter({
+  path: './result.csv',
+  header: [
+    { id: 'email', title: 'Email' },
+    { id: 'password', title: 'Password' },
+    { id: 'proxy', title: 'Proxy' },
+  ],
+  append: true,
+});
+
+function delay(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+const numThreads = config.numThreads;
+const customDelay = config.customDelay;
+
+function parseEmails(filePath: string) {
+  const content = fs.readFileSync(filePath, 'utf8');
+  const lines = content.split('\n');
+  const emails: string[] = [];
+
+  lines.forEach((line: string) => {
+    const email = line.trim();
+    if (email) {
+      emails.push(email);
+    }
+  });
+
+  return emails;
+}
+function parseProxies(filePath: string) {
+  const lines = fs.readFileSync(filePath, 'utf8').split('\n');
+  const proxies: string[] = [];
+
+  lines.forEach((line: string) => {
+    const proxy = line.trim();
+    proxies.push(proxy);
+  });
+
+  return proxies;
+}
+const emails = parseEmails('./inputs/emails.txt');
+const proxies = parseProxies('./inputs/proxies.txt');
+
+async function reg(email: any, proxy: string) {
+  const headers = {
+    'user-agent': random().toString(),
+    accept: 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/json',
+    origin: 'https://referlist.co/',
+    referer: 'https://referlist.co/',
+    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+  };
+  const session = axios.create({
+    headers: headers,
+    httpsAgent:
+      config.proxyType === 'http' ? new HttpsProxyAgent(`http://${proxy}`) : new SocksProxyAgent(`socks5://${proxy}`),
+  });
+
+  const data = { email: email, referralSource: config.ref, waitlistName: 'vold' };
+  const res = await session.post('https://referlist.herokuapp.com/api/addtowaitlist', data);
+  console.log(email, res.data.waitlist.title);
+
+  const resultData = [
+    {
+      email: email,
+      proxy: proxy,
+    },
+  ];
+  await csvWriter
+    .writeRecords(resultData)
+    .then(() => {
+      console.log('CSV file has been saved.');
+    })
+    .catch((error: any) => {
+      console.error(error);
+    });
+}
+
+function regRecursive(emails: any, proxies: any, index = 0, numThreads = 4) {
+  if (index >= emails.length) {
+    return;
+  }
+
+  const worker = new Worker(__filename, {
+    workerData: { email: emails[index], proxy: proxies[index] },
+  });
+  worker.on('message', (message: any) => {
+    console.log(message);
+  });
+  worker.on('error', (error: any) => {
+    console.error(error);
+  });
+  worker.on('exit', (code: any) => {
+    if (code !== 0) {
+      console.error(`Thread Exit ${code}`);
+    }
+    regRecursive(emails, proxies, index + numThreads, numThreads);
+  });
+}
+const main = async () => {
+  if (isMainThread) {
+    for (let i = 0; i < numThreads; i++) {
+      await delay(customDelay);
+      regRecursive(emails, proxies, i, numThreads);
+    }
+  } else {
+    await delay(customDelay);
+    const { email, proxy } = workerData;
+    reg(email, proxy);
+  }
+};
+main();
